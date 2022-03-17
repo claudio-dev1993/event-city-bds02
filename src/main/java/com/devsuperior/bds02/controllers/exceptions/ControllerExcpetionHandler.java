cc8de4927300b61192094a54bf5a73fe163c8730
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2022-03-16 22:58:01 -0300

    CityControllerIT tests 4/5 and 5/5 pass

--- src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExcpetionHandler.java
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExcpetionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.bds02.exceptions.DatabaseException;
 import com.devsuperior.bds02.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,6 +23,18 @@ public class ControllerExcpetionHandler {
 				Instant.now()
 				, status.value()
 				,"Resource not found!" 
+				, e.getMessage(), req.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<BadRequestError> entityNotFound(DatabaseException e, HttpServletRequest req){
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		BadRequestError err = new BadRequestError(
+				Instant.now()
+				, status.value()
+				,"Bad request for independent Id!" 
 				, e.getMessage(), req.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
